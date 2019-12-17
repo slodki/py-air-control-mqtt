1cddf4d61d91838a9d5bf6fb3a1700c3e76bea59
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2019-12-17 14:09:05 +0100

    g

--- pyairctrl/airctrl.py
+++ pyairctrl/airctrl.py
@@ -4,9 +4,7 @@ from Cryptodome.Util.Padding import pad, unpad
 from distutils.util import strtobool
 import requests
 import base64
-import binascii
 import argparse
-import copy
 import json
 import random
 import os
@@ -77,7 +75,7 @@ class AirClient(object):
     }
 
     @staticmethod
-    def ssdp(timeout=1, repeats=3, debug=False):
+    def ssdp(timeout=1, repeats=3):
         addr = '239.255.255.250'
         port = 1900
         msg = '\r\n'.join([
@@ -96,6 +94,7 @@ class AirClient(object):
                 try:
                     while True:
                         data, (ip, _) = s.recvfrom(1024)
+                        logging.debug(data)
                         url = next((x for x in data.decode('ascii').splitlines() if x.startswith('LOCATION: ')), None)
                         urls.update({ip: url[10:]})
                 except socket.timeout:
@@ -131,7 +130,7 @@ class AirClient(object):
         G = int('A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5', 16)
         P = int('B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371', 16)
 
-        print('Exchanging secret key with the device ...')
+        logging.info('Exchanging secret key with the device ...')
         a = random.getrandbits(256)
         A = pow(G, a, P)
         data = json.dumps({'diffie': format(A, 'x')})
@@ -151,9 +150,9 @@ class AirClient(object):
         config.read(fpath)
         if 'keys' not in config.sections():
             config['keys'] = {}
-        hex_key = binascii.hexlify(self._session_key).decode('ascii')
+        hex_key = self._session_key.hex()
         config['keys'][self._host] = hex_key
-        print(f'Saving session_key {hex_key} to {fpath}')
+        logging.info(f'Saving session_key {hex_key} to {fpath}')
         with open(fpath, 'w') as f:
             config.write(f)
 
@@ -166,9 +165,8 @@ class AirClient(object):
                 hex_key = config['keys'][self._host]
                 self._session_key = bytes.fromhex(hex_key)
 
-    def set_values(self, values, debug=False):
-        status = self._put('1/air', values, encrypted=True)
-        self._dump_status(status, debug=debug)
+    def set_values(self, values):
+        return self._put('1/air', values, encrypted=True)
 
     def set_wifi(self, ssid, pwd):
         values = {}
@@ -176,75 +174,82 @@ class AirClient(object):
             values['ssid'] = ssid
         if pwd:
             values['password'] = pwd
-        logging.debug(values)
-        wifi = self._put('0/wifi', values, encrypted=True)
-        logging.debug(wifi)
+        self._put('0/wifi', values, encrypted=True)
 
     def _get_once(self, endpoint):
         response = requests.get(f'http://{self._host}/di/v1/products/{endpoint}')
         response.raise_for_status()
         resp = decrypt(response.text, self._session_key)
-        return json.loads(resp)
+        jsonresp = json.loads(resp)
+        logging.debug(jsonresp)
+        return jsonresp;
 
     def _get(self, endpoint):
         try:
             return self._get_once(endpoint)
         except Exception as e:
-            print(f'Cannot read from device: {type(e).__name__}: {e}')
-        print('Will retry after getting a new key ...')
+            logging.warning(f'Cannot read from device: {type(e).__name__}: {e}')
+        logging.warning('Will retry after getting a new key ...')
         self._get_key()
         return self._get_once(endpoint)
 
     def _put(self, endpoint, body, encrypted=False):
+        logging.debug(body)
         if encrypted:
             body = encrypt(body, self._session_key)
         url = f'http://{self._host}/di/v1/products/{endpoint}'
         response = requests.put(url, body)
         response.raise_for_status()
-        if response.status_code == 255 and len(response.text) == 1:
-            raise ValueError('An option not supported by device')
+        assert response.status_code != 255 and len(response.text) != 1, \
+            'An option not supported by device'
         resp = response.text
         if encrypted:
             resp = decrypt(resp, self._session_key)
-        return json.loads(resp)
+        jsonresp = json.loads(resp)
+        logging.debug(jsonresp)
+        return jsonresp
 
-    def _dump_status(self, status, debug=False):
-        logging.debug(status)
+    def print_status(self, status):
         for opt, val in status.items():
             opt_def = self.options.get(opt, None)
             if opt_def:
+                desc = opt_def.get('desc', 'unknown')
                 val_str = opt_def.get('values',{}).get(val, val)
                 unit = opt_def.get('unit','') if isinstance(val_str, int) else ''
-                print(f'[{opt:5}] {opt_def["desc"]}: {val_str}{unit}')
+                print(f'[{opt:5}] {desc}: {val_str}{unit}')
             else:
                 print(f'[{opt:5}] unknown: {val}')
 
-    def get_status(self, debug=False):
-        status = self._get('1/air')
-        self._dump_status(status, debug=debug)
+    def get_status(self):
+        return self._get('1/air')
 
     def get_wifi(self):
-        wifi = self._get('0/wifi')
-        print(wifi)
+        return self._get('0/wifi')
 
     def get_firmware(self):
-        firmware = self._get('0/firmware')
-        print(firmware)
+        return self._get('0/firmware')
 
     # endpoint '1/userinfo' ?
 
     def get_filters(self):
-        filters = self._get('1/fltsts')
-        print(f'Pre-filter and Wick: clean in {filters["fltsts0"]} hours')
-        if 'wicksts' in filters:
-            print(f'Wick filter: replace in {filters["wicksts"]} hours')
-        print(f'Active carbon filter: replace in {filters["fltsts2"]} hours')
-        print(f'HEPA filter: replace in {filters["fltsts1"]} hours')
+        return self._get('1/fltsts')
+        
+    def print_filters(self):
+        ftypes = { 'A3': 'Active carbon filter', 'C7': 'HEPA filter'}
+        resp = self.get_filters()
+        for f in range(3):
+            time = resp.get(f'fltsts{f}', None)
+            if time:
+                fcode = resp.get(f'fltt{f}', None)
+                ftype = ftypes.get(fcode, f'Unknown {f} filter' if f else 'Pre-filter (and wick)')
+                action = "replace" if f else "clean"
+                print(f'{ftype}: {action} in {time} hours')
+        if 'wicksts' in resp:
+            print(f'Humidification wick: replace in {resp["wicksts"]} hours')
 
     def pair(self, client_id, client_secret):
         values = {'Pair': ['FI-AIR-AND', client_id, client_secret] }
         resp = self._put('0/pairing', values, encrypted=True)
-        print(resp)
 
 
 def main():
@@ -252,6 +257,7 @@ def main():
     parser.register('type', 'bool', (lambda x: bool(strtobool(x))))
     parser.add_argument('--ipaddr', help='IP address of air purifier', action='append')
     parser.add_argument('-d', '--debug', help='show debug output', action='store_true')
+    parser.add_argument('-v', '--verbose', help='show verbose output', action='store_true')
     parser.add_argument('--om', help='set fan speed', choices=['1','2','3','s','t'])
     parser.add_argument('--pwr', help='power on/off', choices=['0','1'])
     parser.add_argument('--mode', help='set mode', choices=['P','A','S','M','B','N'])
@@ -270,38 +276,45 @@ def main():
     parser.add_argument('--filters', help='read filters status', action='store_true')
     args = parser.parse_args()
 
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    elif args.verbose:
+        logging.basicConfig(level=logging.INFO)
+
     if args.ipaddr:
         devices = [ {'ip': ip} for ip in args.ipaddr ]
     else:
-        devices = AirClient.ssdp(debug=args.debug)
+        devices = AirClient.ssdp()
         if not devices:
-            print('Air purifier not autodetected. Try --ipaddr option to force specific IP address.')
+            logging.critical('Air purifier not autodetected. Try --ipaddr option to force specific IP address.')
             sys.exit(1)
     
     for device in devices:
         c = AirClient(device['ip'])
         print(f'{c.name}: (model {c.model}, IP {device["ip"]})')
         if args.wifi:
-            c.get_wifi()
+            for k,v in c.get_wifi().items():
+                print(f'{k}: {v}')
             sys.exit(0)
         if args.firmware:
-            c.get_firmware()
+            for k,v in c.get_firmware().items():
+                print(f'{k}: {v}')
             sys.exit(0)
         if args.wifi_ssid or args.wifi_pwd:
             c.set_wifi(args.wifi_ssid, args.wifi_pwd)
             sys.exit(0)
         if args.filters:
-            c.get_filters()
+            c.print_filters()
             sys.exit(0)
 
-        r_opts = ['ipaddr', 'debug', 'firmware', 'filters', 'wifi', 'wifi-ssid', 'wifi-pws']
+        r_opts = ['ipaddr', 'debug', 'verbose', 'firmware', 'filters', 'wifi', 'wifi-ssid', 'wifi-pws']
         values = dict(filter(lambda x: x[1] != None and x[0] not in r_opts, vars(args).items()))
 
         if values:
-            c.set_values(values, debug=args.debug)
+            resp = c.set_values(values)
         else:
-            c.get_status(debug=args.debug)
-
+            resp = c.get_status()
+        c.print_status(resp)
 
 if __name__ == '__main__':
     main()
